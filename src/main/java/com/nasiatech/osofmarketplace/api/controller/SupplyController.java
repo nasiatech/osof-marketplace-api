46b510ece97b0b8855ad07f5dad474b066822e29
Tanju Brunostar <tanjubrunostar0@gmail.com>
2022-08-21 18:54:21 +0100

    created supplier and supply controllers

--- src/main/java/com/nasiatech/osofmarketplace/api/controller/SupplyController.java
+++ src/main/java/com/nasiatech/osofmarketplace/api/controller/SupplyController.java
@@ -1,4 +1,12 @@
 package com.nasiatech.osofmarketplace.api.controller;
 
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/supply")
 public class SupplyController {
 }
--- src/main/java/com/nasiatech/osofmarketplace/api/controller/ToolController.java
+++ src/main/java/com/nasiatech/osofmarketplace/api/controller/ToolController.java
@@ -6,6 +6,6 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/api/user")
+@RequestMapping("/api/tool")
 public class ToolController {
 }
