240af25d6f6287758d9cd6cfc2617e751d069998
Elroy Kanye <elroykanye@gmail.com>
2022-08-21 17:41:22 +0100

    update entities

--- src/main/java/com/nasiatech/osofmarketplace/data/entity/Product.java
+++ src/main/java/com/nasiatech/osofmarketplace/data/entity/Product.java
@@ -1,3 +1,13 @@
+package com.nasiatech.osofmarketplace.data.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
 @Getter
 @Setter
 @Builder
