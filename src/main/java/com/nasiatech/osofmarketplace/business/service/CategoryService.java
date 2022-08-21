8c84ec9933dc1884cf2c2abc9aa648d1b4caf18e
Elroy Kanye <elroykanye@gmail.com>
2022-08-21 17:55:04 +0100

    add services

---
+++ src/main/java/com/nasiatech/osofmarketplace/business/service/CategoryService.java
@@ -0,0 +1,11 @@
+package com.nasiatech.osofmarketplace.business.service;
+
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+
+@Service
+@RequiredArgsConstructor
+public class CategoryService {
+}
---
+++ src/main/java/com/nasiatech/osofmarketplace/business/service/FarmerService.java
@@ -0,0 +1,9 @@
+package com.nasiatech.osofmarketplace.business.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class FarmerService {
+}
