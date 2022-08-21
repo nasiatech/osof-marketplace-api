470298307b471b7aef6a860d958b5c2965ab6fc9
Elroy Kanye <elroykanye@gmail.com>
2022-08-21 16:54:55 +0100

    add controllers and repositories

---
+++ src/main/java/com/nasiatech/osofmarketplace/data/repo/ConsumerRepository.java
@@ -0,0 +1,8 @@
+package com.nasiatech.osofmarketplace.data.repo;
+
+import com.nasiatech.osofmarketplace.data.entity.Consumer;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ConsumerRepository extends JpaRepository<Consumer, Integer> {
+
+}
---
+++ src/main/java/com/nasiatech/osofmarketplace/data/repo/SupplyRepository.java
@@ -0,0 +1,8 @@
+package com.nasiatech.osofmarketplace.data.repo;
+
+import com.nasiatech.osofmarketplace.data.entity.Supply;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface SupplyRepository extends JpaRepository<Supply, Integer> {
+
+}
