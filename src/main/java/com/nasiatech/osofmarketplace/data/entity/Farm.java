d03233ecf290a9b9195eaee7910bf765f54eac69
andrew21-mch <nfonandrew73@gmail.com>
2022-08-21 05:19:29 +0100

    setup the entity for Consumer and Farm

--- src/main/java/com/nasiatech/osofmarketplace/data/entity/Farm.java
+++ src/main/java/com/nasiatech/osofmarketplace/data/entity/Farm.java
@@ -1,1 +1,20 @@
-public class Farm{private Integer id;}
+package com.nasiatech.osofmarketplace.data.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+public class Farm{
+    @Id
+    private Integer id;
+}
